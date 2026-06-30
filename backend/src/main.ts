40a0de1ea7a527dcc7d9c5d2e6943603c0ece70a
Adamu Muhammad <sayyid211@gmail.com>
2026-06-30 23:20:37 +0300

    update CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,8 +6,12 @@ async function bootstrap() {
 
   // 1. Enable CORS so the Next.js frontend can communicate with this API
   app.enableCors({
-    origin: '*', // For development, allow all origins. You can restrict this to 'http://localhost:3000' later.
+    origin: [
+      'https://elazigilahiyatveharputilimvakfi.org',
+      'https://www.elazigilahiyatveharputilimvakfi.org',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   // 2. Explicitly set the backend to run on port 4000 to match our frontend api.ts
